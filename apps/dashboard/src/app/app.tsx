2f18b35c4cea0f2d0fb8b7a50a12314216186b16
ricardonava <ricardo.a.nava@gmail.com>
2024-01-30 11:42:58 -0600

    feat: setup main app and authentication wrapper

--- apps/dashboard/src/app/app.tsx
+++ apps/dashboard/src/app/app.tsx
@@ -1,7 +1,43 @@
 import { Authentication } from '@directorio/dashboard/authentication';
+import { supabase } from '@directorio/supabase-js';
+import { HomeIcon, UserIcon } from '@heroicons/react/24/outline';
+import { Session } from '@supabase/supabase-js';
+import { useEffect, useState } from 'react';
+import Layout from './Layout';
+import Account from './Account';
+
+const navigation = [
+  // { name: 'Inicio', href: '#', icon: HomeIcon, current: true },
+  { name: 'Perfil', href: '#', icon: UserIcon, current: true },
+];
 
 export function App() {
-  return <Authentication />;
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  return (
+    <>
+      {!session ? (
+        <Authentication />
+      ) : (
+        <Layout
+          navigation={navigation}
+          userEmail={session.user.email || 'Anonymous'}
+        >
+          <Account />
+        </Layout>
+      )}
+    </>
+  );
 }
 
 export default App;
--- libs/dashboard/authentication/src/lib/authentication.tsx
+++ libs/dashboard/authentication/src/lib/authentication.tsx
@@ -1,22 +1,37 @@
+import { supabase } from '@directorio/supabase-js';
+import { FormEvent, useState } from 'react';
+
 /* eslint-disable-next-line */
 export interface AuthenticationProps {}
 
 export function Authentication(props: AuthenticationProps) {
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+
+  const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setLoading(true);
+    const { error } = await supabase.auth.signInWithOtp({ email });
+
+    if (error) {
+      alert(error.message);
+    } else {
+      alert('Check your email for the login link!');
+    }
+    setLoading(false);
+  };
+
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img
-          className="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
-        />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Sign in to your account
         </h2>
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
+        <form className="space-y-6" onSubmit={handleLogin}>
           <div>
             <label
               htmlFor="email"
@@ -32,16 +47,18 @@ export function Authentication(props: AuthenticationProps) {
                 autoComplete="email"
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
           </div>
 
           <div>
             <button
+              disabled={loading}
               type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 disabled:bg-indigo-500 disabled:cursor-not-allowed focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Send magic link
+              {loading ? <span>Loading</span> : <span>Send magic link</span>}
             </button>
           </div>
         </form>
